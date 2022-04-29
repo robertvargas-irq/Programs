108b1b29a3af0ac16f9275dbe81777e11d881980
robertvargas-irq <90274287+robertvargas-irq@users.noreply.github.com>
2022-04-29 00:14:58 -0600

    Created test file for Circle2

---
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -0,0 +1,57 @@
+package edu.nmsu.cs.circles;
+import org.junit.*;
+
+public class Circle2Test {
+	// Data you need for each test case
+	private Circle2 circle2;
+
+	//
+	// Stuff you want to do before each test case
+	//
+	@Before
+	public void setup()
+	{
+		System.out.println("\nTest starting...");
+		circle2 = new Circle2(1, 2, 3);
+	}
+
+	//
+	// Stuff you want to do after each test case
+	//
+	@After
+	public void teardown()
+	{
+		System.out.println("\nTest finished.");
+	}
+
+	//
+	// Test a simple positive move
+	//
+	@Test
+	public void simpleMove()
+	{
+		Point p;
+		System.out.println("Running test simpleMove.");
+		p = circle2.moveBy(1, 1);
+		Assert.assertTrue(p.x == 2 && p.y == 3);
+	}
+
+	//
+	// Test a simple negative move
+	//
+	@Test
+	public void simpleMoveNeg()
+	{
+		Point p;
+		System.out.println("Running test simpleMoveNeg.");
+		p = circle2.moveBy(-1, -1);
+		Assert.assertTrue(p.x == 0 && p.y == 1);
+	}
+
+	/***
+	 * NOT USED public static void main(String args[]) { try { org.junit.runner.JUnitCore.runClasses(
+	 * java.lang.Class.forName("Circle2Test")); } catch (Exception e) { System.out.println("Exception:
+	 * " + e); } }
+	 ***/
+
+}
